b46f9d88755d1febfd213eb643129ae498088a9c
deepakmaggo-hq <deepak.maggo@myhq.in>
2026-06-26 11:11:20 +0530

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -28,7 +28,7 @@ app = FastAPI(title="Email Agent API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://172.22.0.1:3000"], 
+    allow_origins=["http://localhost:3000", "http://172.22.0.1:3000","http://192.168.20.184:3000"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
